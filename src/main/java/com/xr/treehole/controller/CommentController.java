8c5f63a466abddabd863de4cc534f3311018ab6f
Xylon <x956200361@gmail.com>
2020-12-16 23:15:35 +0800

    merge comment controller into node controller

--- src/main/java/com/xr/treehole/controller/CommentController.java
+++
@@ -1,9 +0,0 @@
-package com.xr.treehole.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-@Controller
-@RequestMapping(path = "/comment")
-public class CommentController {
-
-}
