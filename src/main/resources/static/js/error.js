10bfbb4c46094a9cb2080f09d60e52ac2824e0cd
Rapiz1 <contact@rapiz.me>
2020-12-19 12:47:13 +0800

    Shorten jump time of invalid tokens

--- src/main/resources/static/js/error.js
+++ src/main/resources/static/js/error.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
     var jwtToken = $.cookie("JwtToken");
     var errorInfo = $("#errorInfo")
     if (jwtToken === undefined || jwt_decode(jwtToken)["exp"] < Math.round((new Date()).getTime() / 1000)) {
-        var $notLoginInfo = $("<h3 class='center-align'>未登录或令牌过期, 3s后自动跳转至登录页面......</h3>")
+        var $notLoginInfo = $("<h3 class='center-align'>未登录或令牌过期, 1s后自动跳转至登录页面......</h3>")
         errorInfo.append($notLoginInfo)
         setTimeout(function () {
             $(location).attr("href", "/user/login")
-        }, 3000)
+        }, 1000)
     } else {
         var $unknown = $("<h3 class='center-align'>未知错误，3s后自动跳转至主界面...</h3>")
         errorInfo.append($unknown)
