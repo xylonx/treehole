d9930da8d07937983d28b2b677a15571d322c953
Xylon <x956200361@gmail.com>
2020-12-22 15:10:44 +0800

    add jquery cookie cdn

--- src/test/java/com/xr/treehole/TreeholeApplicationTests.java
+++ src/test/java/com/xr/treehole/TreeholeApplicationTests.java
@@ -1,6 +1,8 @@
 package com.xr.treehole;
 
+import com.xr.treehole.middleware.jwt.JwtUtils;
 import com.xr.treehole.service.MailService;
+import com.xr.treehole.util.Encrypt;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,13 +13,18 @@ class TreeholeApplicationTests {
     @Autowired
     private MailService mailService;
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+//    private JwtUtils    jwtUtils;
 
     @Test
-    void SendTemplateMail(){
-        mailService.SendTemplateMail("u201917285@hust.edu.cn", "Bot", "123456");
+    void contextLoads() {
+        assert Encrypt.CheckSecurityHash("15948", "$2a$10$7s1M4SU6pS1HzLYy3HIzV.XPbZIBqwJqmsk.vGAbPZ31j.bSulXWa");
+        assert Encrypt.Hashing("3102131813@qq.com").equals("88346234510054dd998fdff07f5e389a0af11912720692bff87eac75c1115a23");
     }
+//
+//    @Test
+//    void SendTemplateMail(){
+//        mailService.SendTemplateMail("u201917285@hust.edu.cn", "Bot", "123456");
+//    }
 
 }
