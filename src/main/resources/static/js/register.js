4cbe939f616f988c98b311e520a03ee91ad0d017
Xylon <x956200361@gmail.com>
2020-12-22 16:27:56 +0800

    set register code getting time duration

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     $("#GetRegisterCode").click(function () {
+        var email_pattern = /^.*@.*$/
         var emailAddress = $("#emailAddress").val()
+        if (email_pattern.test(emailAddress) === false) return
         $.post(
             "/user/register-code",
             {
@@ -10,5 +12,8 @@ $(document).ready(function () {
                 if (data === "re register") $(location).attr("href", "/user/login")
             }
         )
+        $(this).hide()
+        alert("已发送注册码至您的邮件，请注意查收。再次获取注册码请在1分钟后进行")
+        setTimeout(function (){$(this).show()}, 1000*60)
     })
 })
